7319cd603f6e6ef13335ee16332582539d2f3f55
antonchernik <anton@chernik.info>
2015-10-31 20:16:57 +0200

    Add vim configuaration

--- debian7setup.sh
+++ debian7setup.sh
@@ -2,15 +2,18 @@ apt-get update; apt-get upgrade -y;
 apt-get -y install vim htop cron zip unzip wget curl mc apache2-utils
 update-alternatives --set editor /usr/bin/vim.basic
 locale-gen "ru_RU.UTF-8"
-export LANGUAGE=ru_RU.UTF-8 && \
-	export LANG=ru_RU.UTF-8 && \
-	export LC_ALL=ru_RU.UTF-8 && \
-	locale-gen ru_RU.UTF-8 && \
-	--frontend=noninteractive dpkg-reconfigure locales
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
+sed -i -e 's/# ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen && \
+echo 'LANG="ru_RU.UTF-8"'>/etc/default/locale && \
+dpkg-reconfigure --frontend=noninteractive locales && \
+update-locale LANG=ru_RU.UTF-8
 echo "Europe/Kiev" > /etc/timezone && \
 dpkg-reconfigure -f noninteractive tzdata
 
+sed -i -e 's/"syntax on/syntax on\ncolorscheme ron\nset number/' /etc/vim/vimrc
+echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" > ~/.bashrc
 
+apt-get -y install bsdutils build-essential libaio1 libssl-dev libcurl4-openssl-dev libevent-dev sendmail-bin sensible-mda
 apt-get -y install module-init-tools
 apt-get -y install php5-cli php-pear php5-curl php5-gd php5-mcrypt php5-dev php5-intl php5-fpm memcached php5-memcached php5-xsl imagemagick php5-imagick
 sed -i 's/\;date\.timezone\ \=/date\.timezone\ \=\ Europe\/Kiev/g' /etc/php5/cli/php.ini
