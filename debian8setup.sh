70f97127ed62f0e69f38c537affd56950114d899
antonchernik <anton@chernik.info>
2015-10-31 22:13:01 +0200

    Add Mysql, Mongo and Media reposiory

--- debian8setup.sh
+++ debian8setup.sh
@@ -5,6 +5,8 @@ USER=user
 GROUP=user
 #Debian user password
 PASSWORD=access
+#Mysql root password
+MYSQL_ROOT_PASSWORD=access
 #If user does not exists create it
 id -u $USER &>/dev/null || adduser $USER --disabled-password --gecos "" && echo "$USER:$PASSWORD" | chpasswd
 apt-get update; apt-get upgrade -y;
@@ -20,7 +22,7 @@ echo "Europe/Kiev" > /etc/timezone && \
 dpkg-reconfigure -f noninteractive tzdata
 
 sed -i -e 's/"syntax on/syntax on\ncolorscheme ron\nset number/' /etc/vim/vimrc
-echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\# '" >> ~/.bashrc
+echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u\[\033[01;33m\]@\[\033[01;36m\]\h \[\033[01;33m\]\w \[\033[01;35m\]\$ \[\033[00m\]'" >> ~/.bashrc
 
 apt-get -y install bsdutils build-essential libaio1 libssl-dev libcurl4-openssl-dev libevent-dev sendmail-bin sensible-mda
 apt-get -y install module-init-tools
@@ -34,6 +36,9 @@ sed -i "s/short_open_tag = .*/short_open_tag = On/" /etc/php5/fpm/php.ini
 sed -i "s/short_open_tag = .*/short_open_tag = On/" /etc/php5/cli/php.ini
 sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini
 sed -i "s/;opcache.enable=0/opcache.enable=0/" /etc/php5/fpm/php.ini
+sed -i -e 's/-m 64/-m 256/' /etc/memcached.conf
+/etc/init.d/memcached restart
+
 
 curl -sS https://getcomposer.org/installer | php && \
 mv composer.phar /usr/local/bin/composer
@@ -61,5 +66,31 @@ pecl install gearman
 echo "extension=gearman.so" | tee /etc/php5/mods-available/gearman.ini
 ln -s /etc/php5/mods-available/gearman.ini /etc/php5/cli/conf.d/20-gearman.ini
 ln -s /etc/php5/mods-available/gearman.ini /etc/php5/fpm/conf.d/20-gearman.ini
+/etc/init.d/gearman-job-server restart
+/etc/init.d/php5-fpm restart
+
+echo "deb-src http://repo.mysql.com/apt/debian/ jessie mysql-5.7" >> /etc/apt/sources.list.d/mysql.list
+echo "deb http://repo.mysql.com/apt/debian/ jessie mysql-5.7"  >> /etc/apt/sources.list.d/mysql.list
+apt-get update; apt-get upgrade -y;
+
+echo "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD" | debconf-set-selections 
+echo "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" | debconf-set-selections 
+apt-get -y install mysql-server
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.0 main" | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+apt-get update; apt-get upgrade -y;
+apt-get install -y mongodb-org
+service mongod start
+pecl install mongo
+echo "extension=mongo.so" | tee /etc/php5/mods-available/mongo.ini
+ln -s /etc/php5/mods-available/mongo.ini /etc/php5/cli/conf.d/20-mongo.ini
+ln -s /etc/php5/mods-available/mongo.ini /etc/php5/fpm/conf.d/20-mongo.ini
+
+echo "deb http://www.deb-multimedia.org wheezy main non-free" >> /etc/apt/sources.list.d/deb-multimedia.list && \
+echo "deb-src http://www.deb-multimedia.org wheezy main non-free" >> /etc/apt/sources.list.d/deb-multimedia.list && \
+apt-get update && \
+apt-get install deb-multimedia-keyring
+
 
 
