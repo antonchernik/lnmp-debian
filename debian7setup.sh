a9be323b61ad9b7c894e66b6e299b61a403b22cc
antonchernik <anton@chernik.info>
2015-11-16 20:45:17 +0200

    Update debian7setup.sh

--- debian7setup.sh
+++ debian7setup.sh
@@ -21,7 +21,7 @@ sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
 echo 'LANG="en_US.UTF-8"'>/etc/default/locale && \
 dpkg-reconfigure --frontend=noninteractive locales && \
 update-locale LANG=ru_RU.UTF-8
-echo "Europe/Kiev" > /etc/timezone && \
+echo "Asia/Tel_Aviv" > /etc/timezone && \
 dpkg-reconfigure -f noninteractive tzdata
 
 sed -i -e 's/"syntax on/syntax on\ncolorscheme ron\nset number/' /etc/vim/vimrc
@@ -29,8 +29,8 @@ sed -i -e 's/"syntax on/syntax on\ncolorscheme ron\nset number/' /etc/vim/vimrc
 apt-get -y install bsdutils build-essential libaio1 libssl-dev libcurl4-openssl-dev libevent-dev sendmail-bin sensible-mda
 apt-get -y install module-init-tools
 apt-get -y install php5-cli php-pear php5-curl php5-gd php5-mcrypt php5-dev php5-intl php5-fpm memcached php5-memcached php5-xsl imagemagick php5-imagick
-sed -i 's/\;date\.timezone\ \=/date\.timezone\ \=\ Israel/g' /etc/php5/cli/php.ini
-sed -i 's/\;date\.timezone\ \=/date\.timezone\ \=\ Israel/g' /etc/php5/fpm/php.ini
+sed -i 's/\;date\.timezone\ \=/date\.timezone\ \=\ Asia\/Tel_Aviv/g' /etc/php5/cli/php.ini
+sed -i 's/\;date\.timezone\ \=/date\.timezone\ \=\ Asia\/Tel_Aviv/g' /etc/php5/fpm/php.ini
 sed -i "s/max_execution_time = .*/max_execution_time = 60/" /etc/php5/fpm/php.ini
 sed -i "s/upload_max_filesize = .*/upload_max_filesize = 32M/" /etc/php5/fpm/php.ini
 sed -i "s/post_max_size = .*/post_max_size = 32M/" /etc/php5/fpm/php.ini
