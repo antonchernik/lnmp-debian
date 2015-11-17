2eb41a6d89a2f6dd019598c72086096296504625
antonchernik <anton@chernik.info>
2015-11-17 11:47:41 +0200

    Update debian7setup.sh

--- debian7setup.sh
+++ debian7setup.sh
@@ -22,7 +22,7 @@ locale-gen "en_US.UTF-8"
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
 echo 'LANG="en_US.UTF-8"'>/etc/default/locale && \
 dpkg-reconfigure --frontend=noninteractive locales && \
-update-locale LANG=ru_RU.UTF-8
+update-locale LANG=en_US.UTF-8
 echo "Asia/Tel_Aviv" > /etc/timezone && \
 dpkg-reconfigure -f noninteractive tzdata
 
